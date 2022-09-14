82e1e524924ee34d380d7cf2107ab39646861c95
pulokmehrab <khandokarmeherab@gmail.com>
2022-09-14 21:14:51 +0600

    start button fixed

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const display = document.getElementById("display");
 const question = document.getElementById("question");
-const startBtn = document.getElementById("start");
+const startBtn = document.getElementById("starts");
 const countdownOverlay = document.getElementById("countdown");
 const resultModal = document.getElementById("result");
 const modalBackground = document.getElementById("modal-background");
 
 // variables
 let userText = "";
-let errorCount = 0;
+let errorCount = 0; 
 let startTime;
 let questionText = "";
 
